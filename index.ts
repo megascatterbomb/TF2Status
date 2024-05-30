3a3f384699375bcbba2999743845f705d9ff5947
megascatterbomb <megascatterbomb@gmail.com>
2024-05-30 15:43:28 +1200

    fix off by one

--- index.ts
+++ index.ts
@@ -194,7 +194,7 @@ function buildServerActivity(resultArchive: Result[]): string {
         newOutput += playerGraphString;
         newOutput += playerCountString;
         
-        if(newOutput.length > maxCharsFieldValue - outputEnd.length || newOutput.split("\n").length > maxRows + 1) {
+        if(newOutput.length > maxCharsFieldValue - outputEnd.length || newOutput.split("\n").length > maxRows + 2) {
             break;
         }
         output = newOutput;
